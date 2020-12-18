dbd2fcc4db7e433121400e09924545b56e0c713f
liyachao <liyachao@rongcloud.cn>
2020-12-18 13:20:26 +0800

    优化 log 显示

--- demo/instrument_services/power.py
+++ demo/instrument_services/power.py
@@ -4,7 +4,9 @@ import os
 import sys
 sys.path.append(os.getcwd())
 from instrument.RPC import get_usb_rpc
+from util import logging
 
+log = logging.getLogger(__name__)
 
 def power(rpc):
     headers = ['startingTime', 'duration', 'level']  # DTPower
@@ -13,7 +15,7 @@ def power(rpc):
     }
 
     def on_channel_message(res):
-        print(res.parsed)
+        log.debug(res.parsed)
         ctx['remained'] += res.parsed['data']
         cur = 0
         while cur + 3 * 8 <= len(ctx['remained']):
@@ -28,11 +30,11 @@ def power(rpc):
     channel = "com.apple.instruments.server.services.power"
     rpc.register_channel_callback(channel, on_channel_message)
     stream_num = rpc.call(channel, "openStreamForPath:", "live/level.dat").parsed
-    print("open", stream_num)
-    print("start", rpc.call(channel, "startStreamTransfer:", float(stream_num)).parsed)
-    print("[!] wait a few seconds, be patient...")
+    log.debug("open", stream_num)
+    log.debug("start", rpc.call(channel, "startStreamTransfer:", float(stream_num)).parsed)
+    log.debug("[!] wait a few seconds, be patient...")
     time.sleep(10)
-    print("stop", rpc.call(channel, "endStreamTransfer:", float(stream_num)).parsed)
+    log.debug("stop", rpc.call(channel, "endStreamTransfer:", float(stream_num)).parsed)
     rpc.stop()
 
 
